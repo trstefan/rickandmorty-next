faa76e6d9d59e66ec45752dbbfce55d2214a12b9
xStef23 <trstefy@gmail.com>
2025-04-04 18:49:35 +0100

    small typo

--- src/app/characters/page.js
+++ src/app/characters/page.js
@@ -136,8 +136,8 @@ export default function Characters() {
             No characters found
           </h3>
           <p className="text-gray-600">
-            Try adjusting your search or filters to find what you're looking
-            for.
+            Try adjusting your search or filters to find what you&apos;re
+            looking for.
           </p>
         </div>
       )}
